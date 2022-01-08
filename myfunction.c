faee1d06cec135521ec7cb14f0423273f1a4a56f
Eliran Eiluz <eliranfr1@gmail.com>
2022-01-08 22:32:44 +0200

    gg

--- myfunction.c
+++ myfunction.c
@@ -18,71 +18,7 @@ typedef struct {
  *  Applies kernel for pixel at (i,j)
  */
 
-static pixel applyKernel(int dim, int i, int j, pixel *src, int kernelSize, int kernel[kernelSize][kernelSize], int kernelScale, bool filter) {
 
-    register int ii, jj, index, index2;
-    pixel current_pixel, pix;
-    int min_intensity = 766; // arbitrary value that is higher than maximum possible intensity, which is 255*3=765
-    int max_intensity = -1; // arbitrary value that is lower than minimum possible intensity, which is 0
-    int min_row, min_col, max_row, max_col;
-    pixel loop_pixel;
-    int kRow, kCol;
-    register int x, y, z;
-    x = y = z = 0;
-    int checker = MIN(i + 1, dim - 1);
-    int checker1 = MIN(j + 1, dim - 1);
-    for (ii = MAX(i - 1, 0); ii <= checker; ii++) {
-        // compute row index in kernel
-        if (ii < i) {
-            kRow = 0;
-        } else if (ii > i) {
-            kRow = 2;
-        } else {
-            kRow = 1;
-        }
-
-        for (jj = MAX(j - 1, 0); jj <= checker1; jj++) {
-
-
-            // compute column index in kernel
-            if (jj < j) {
-                kCol = 0;
-            } else if (jj > j) {
-                kCol = 2;
-            } else {
-                kCol = 1;
-            }
-
-            pix = src[CALCINDEX(ii, jj, dim)];
-            index = kernel[kRow][kCol];
-            x = ADD(x, MULT(pix.red, index));
-            y = ADD(y, MULT(pix.green, index));
-            z = ADD(z, MULT(pix.blue, index));
-        }
-    }
-
-    if (filter) {
-        int toCheck;
-        for (ii = MAX(i - 1, 0); ii <= checker; ii++) {
-            index2 = MULT(ii, dim);
-            for (jj = MAX(j - 1, 0); jj <= checker1; jj++) {
-                // check if smaller than min or higher than max and update
-                loop_pixel = src[ADD(index2, jj)];
-                toCheck = ADD(ADD((loop_pixel.red), (loop_pixel.green)), (loop_pixel.blue));
-                if (toCheck <= min_intensity) {
-                    min_intensity = toCheck;
-                    min_row = ii;
-                    min_col = jj;
-                }
-                if (toCheck > max_intensity) {
-                    max_intensity = (toCheck);
-                    max_row = ii;
-                    max_col = jj;
-                }
-            }
-        }
-    }
-}
 void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sharpRsltImgName, char* filteredBlurRsltImgName, char* filteredSharpRsltImgName, char flag) {
 
     int c = MULT(MULT(m,n),sizeof(pixel));
@@ -112,19 +48,30 @@ void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sh
             w2 = MULT(i, m);
             w3 = MULT(i+1,m);
             for (j = 1 ; j < limit; j++) {
-                //dst[calcIndex(i, j, dim)] = applyKernel(dim, i, j, src, kernelSize, kernel, kernelScale, filter);
                 pixelsImg[ADD(w2,j)].red = DIVIDE((backupOrg[ADD(w1,j-1)].red + backupOrg[ADD(w1,j)].red + backupOrg[ADD(w1,j+1)].red
                                                    + backupOrg[ADD(w2,j-1)].red + backupOrg[ADD(w2,j)].red + backupOrg[ADD(w2,j+1)].red +
                                                    backupOrg[ADD(w3,j-1)].red + backupOrg[ADD(w3,j)].red + backupOrg[ADD(w3,j+1)].red),9);
-                pixelsImg[ADD(w2,j)].green = DIVIDE((backupOrg[ADD(w1,j-1)].green + backupOrg[ADD(w1,j)].green + backupOrg[ADD(w1,j+1)].green
-                                                     + backupOrg[ADD(w2,j-1)].green + backupOrg[ADD(w2,j)].green + backupOrg[ADD(w2,j+1)].green +
-                                                     backupOrg[ADD(w3,j-1)].green + backupOrg[ADD(w3,j)].green + backupOrg[ADD(w3,j+1)].green),9);
+
+                pixelsImg[ADD(w2,j)].green = DIVIDE((backupOrg[ADD(w1,j-1)].green + backupOrg[ADD(w1,j)].green + backupOrg[ADD(w1,j+1)].green +
+                                                   backupOrg[ADD(w2,j-1)].green + backupOrg[ADD(w2,j)].green + backupOrg[ADD(w2,j+1)].green +
+                                                   backupOrg[ADD(w3,j-1)].green + backupOrg[ADD(w3,j)].green + backupOrg[ADD(w3,j+1)].green),9);
+
                 pixelsImg[ADD(w2,j)].blue = DIVIDE((backupOrg[ADD(w1,j-1)].blue + backupOrg[ADD(w1,j)].blue + backupOrg[ADD(w1,j+1)].blue
-                                                    + backupOrg[ADD(w2,j-1)].blue + backupOrg[ADD(w2,j)].blue + backupOrg[ADD(w2,j+1)].blue +
-                                                    backupOrg[ADD(w3,j-1)].blue + backupOrg[ADD(w3,j)].blue + backupOrg[ADD(w3,j+1)].blue),9);
+                                                   + backupOrg[ADD(w2,j-1)].blue + backupOrg[ADD(w2,j)].blue + backupOrg[ADD(w2,j+1)].blue +
+                                                   backupOrg[ADD(w3,j-1)].blue + backupOrg[ADD(w3,j)].blue + backupOrg[ADD(w3,j+1)].blue),9);
             }
         }
-
+        for (row = 0 ; row < m ; row++) {
+            sum1 = MULT(row,n);
+            sum2 = MULT(sum1,3);
+            for (col = 0 ; col < n ; col++) {
+                sum4 = ADD(MULT(3,col),sum2);
+                sum3 = ADD(sum1,col);
+                backupOrg[sum3].red = image->data[sum4] = pixelsImg[sum3].red;
+                backupOrg[sum3].green = image->data[ADD(sum4,1)] = pixelsImg[sum3].green;
+                backupOrg[sum3].blue = image->data[ADD(sum4,2)] = pixelsImg[sum3].blue;
+            }
+        }
         // write result image to file
         writeBMP(image, srcImgpName, blurRsltImgName);
 
@@ -169,20 +116,23 @@ void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sh
 
             }
         }
+        for (row = 0 ; row < m ; row++) {
+            sum1 = MULT(row,n);
+            sum2 = MULT(sum1,3);
+            for (col = 0 ; col < n ; col++) {
+                sum4 = ADD(MULT(3,col),sum2);
+                sum3 = ADD(sum1,col);
+                backupOrg[sum3].red = image->data[sum4] = pixelsImg[sum3].red;
+                backupOrg[sum3].green = image->data[ADD(sum4,1)] = pixelsImg[sum3].green;
+                backupOrg[sum3].blue = image->data[ADD(sum4,2)] = pixelsImg[sum3].blue;
+            }
+        }
         // write result image to file
         writeBMP(image, srcImgpName, filteredBlurRsltImgName);
     }
-    for (row = 0 ; row < m ; row++) {
-        sum1 = MULT(row,n);
-        sum2 = MULT(sum1,3);
-        for (col = 0 ; col < n ; col++) {
-            sum4 = ADD(MULT(3,col),sum2);
-            sum3 = ADD(sum1,col);
-            backupOrg[sum3].red = image->data[sum4] = pixelsImg[sum3].red;
-            backupOrg[sum3].green = image->data[ADD(sum4,1)] = pixelsImg[sum3].green;
-            backupOrg[sum3].blue = image->data[ADD(sum4,2)] = pixelsImg[sum3].blue;
-        }
-    }
+
+    //end of else statement
+
     // sharpen the resulting image
     //doConvolution(image, 3, sharpKernel, 1, false);
     for (i = 1 ; i < limit; i++) {
@@ -202,7 +152,6 @@ void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sh
                                                 backupOrg[ADD(w3,j-1)].blue - backupOrg[ADD(w3,j)].blue - backupOrg[ADD(w3,j+1)].blue,0),255);
         }
     }
-
     for (row = 0 ; row < m ; row++) {
         sum1 = MULT(row,n);
         sum2 = MULT(sum1,3);
@@ -215,8 +164,10 @@ void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sh
         }
     }
 
-    // write result image to file
-    writeBMP(image, srcImgpName, sharpRsltImgName);
+    if(flag == '1')
+        writeBMP(image, srcImgpName, sharpRsltImgName);
+    else
+        writeBMP(image, srcImgpName, filteredSharpRsltImgName);
     free(pixelsImg);
     free(backupOrg);
 }
